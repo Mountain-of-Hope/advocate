e881f3560e47c28ce71a9e7acef9c772e19ceb28
cwallace2 <caleb.wallace@avispl.com>
2023-02-21 17:02:10 -0600

    ui improvements

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,10 +1,17 @@
 # -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
 
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Sponsor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    
+class Payment(models.Model):
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    purpose = models.CharField(max_length=255)
