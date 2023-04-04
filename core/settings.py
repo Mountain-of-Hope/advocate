22a957a21bbbf813879fc5de601dc6118097bb1a
cwallace2 <caleb.wallace@avispl.com>
2023-04-04 11:35:08 -0500

    trigger new build

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ DEBUG = env('DEBUG')
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
 
 # load production server from .env
-ALLOWED_HOSTS        = ['18.206.148.137', '127.0.0.1', 'advocate-env.eba-jks23pkf.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS        = ['18.206.148.137', '127.0.0.1', 'advocate-env.eba-jks23pkf.us-east-1.elasticbeanstalk.com' ]
 #CSRF_TRUSTED_ORIGINS = ['http://localhost:85', 'http://127.0.0.1', 'https://' + env('SERVER', default='127.0.0.1') ]
 
 # Application definition
