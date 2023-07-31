de2b9051deae8a318fbe06c3d0dff5a6e11e5555
cwallace2 <caleb.wallace@avispl.com>
2023-07-31 16:35:49 -0500

    initial model refactor

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,73 +1,52 @@
 # -*- encoding: utf-8 -*-
 
 from django.db import models
-from address.models import AddressField
-from phonenumber_field.modelfields  import PhoneNumberField
-from django.contrib.auth.models import User
-from django import template
 
 # Create your models here.
-
 # Program choices
-PROGRAM_CHOICES = (
-    ("project", "Project"),
-    ("program", "Program"),
+PAYMENT_INTERVALS = (
+    (1, "Monthly"),
+    (3, "Quarterly"),
+    (6, "Biannual"),
+    (12, "Yearly"),
 )
 
-# Program choices
-PAYMENT_CHOICES = (
-    ("Check", "Check"),
-    ("Online", "Online"),
-)
+class Person(models.Model):
+    name = models.CharField(max_length=70)
+    date_of_birth = models.DateField(blank=True, null=True)
+    email = models.EmailField(max_length=254, blank=True)
+    address = models.TextField(blank=True)
+    phone = models.CharField(blank=True)
 
-class Program(models.Model):
-    name = models.CharField(max_length=255)
-    type = models.CharField(max_length=20, choices=PROGRAM_CHOICES, default='program')
-    description = models.CharField(max_length=255, blank=True)
-    cost = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.name
 
-class Church (models.Model):
+class SponsorshipType(models.Model):
+    name = models.CharField(max_length=35)
+    
+class Group(models.Model):
     name = models.CharField(max_length=255)
-    address = AddressField()
-    email = models.EmailField(null=True)
-    phone = PhoneNumberField(blank=True)
+    email = models.EmailField(max_length=254, blank=True)
+    address = models.TextField(blank=True)
+    phone = models.CharField(blank=True)
+    members = models.ForeignKey(Person, on_delete=models.CASCADE, blank=True, null=True)
 
-    def __str__(self):
-        return self.name
+class Donation(models.Model):
+    pass
 
-class Donor (models.Model):
-    name = models.CharField(max_length=255)
-    address = AddressField(on_delete=models.CASCADE)
-    email = models.EmailField(null=True)
-    phone = PhoneNumberField(blank=True)
-    church = models.ForeignKey(Church, on_delete=models.CASCADE, null=True, blank=True)
+class Beneficiary(Person):
+    pass
+    
+class Sponsorship(models.Model):
+    type = models.ForeignKey(SponsorshipType, on_delete=models.CASCADE)
+    beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE)
+    interval = models.CharField(max_length=20, choices=PAYMENT_INTERVALS, default='Monthly')
 
-    def __str__(self):
-        return self.name
+class Donor(Person):
+    sponsorship = models.ManyToManyField(Sponsorship, blank=True, null=True)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
+    donations = models.ManyToManyField(Donation,blank=True, null=True)
 
-class Student(models.Model):
-    enroll_date = models.DateField(null=True)
-    name = models.CharField(max_length=255)
-    dob = models.DateField(null=True)
-    community = models.CharField(max_length=255, null=True)
-    program = models.ForeignKey(Program,on_delete=models.CASCADE, null=True, blank=True)
-    grade = models.CharField(max_length=255, null=True)
-    sponsor = models.ManyToManyField(Donor, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-    def GetSponsorName(self):
-        return self.sponsor.name
-
-class Payment(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True, blank=True)
-    donor = models.ForeignKey(Donor, on_delete=models.CASCADE, null=True, blank=True)
-    method = models.CharField(max_length=10, choices=PAYMENT_CHOICES, default='Check')
-    checkNumber = models.IntegerField(default=0)
-    amount = models.DecimalField(max_digits=12, decimal_places=2)
-    duration = models.IntegerField(default=None)
-    date = models.CharField(default="", max_length=255)
