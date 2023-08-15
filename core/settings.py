2894a02aea5b19782f29003c3a9240dfdb74cad2
cwallace2 <caleb.wallace@avispl.com>
2023-08-15 16:43:06 -0500

    disable debug in prod

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY', default='testing123!@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'TRUE'
+DEBUG = 'FALSE'
 # Assets Management
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
