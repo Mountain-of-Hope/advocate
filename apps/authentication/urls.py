860b8f82a54a28b6f668fb1d1be3e9c8b6797199
cwallace2 <caleb.wallace@avispl.com>
2023-08-18 08:50:37 -0500

    temp user

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -9,7 +9,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', login_view, name="login"),
-    path('register/', login_view, name="register"),
+    path('register/', register_user, name="register"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path('social_login/', include('allauth.urls')),
 ]
